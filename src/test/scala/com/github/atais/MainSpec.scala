8e8011b3a7192c92dd129072a7d57eec4c669f73
Michał Siatkowski <atais.jr@gmail.com>
2024-01-31 00:35:39 +0100

    one file

--- src/test/scala/com/github/atais/MainSpec.scala
+++ src/test/scala/com/github/atais/MainSpec.scala
@@ -1,5 +1,9 @@
 package com.github.atais
+
+import com.github.atais.FakeServer.HttpServer
 import okhttp3.{OkHttpClient, Request}
+import zio._
+import zio.http._
 import zio.test._
 
 object MainSpec extends FakeServer {
@@ -17,6 +21,31 @@ object MainSpec extends FakeServer {
 
         assertTrue(response.code() == 200)
       }
-    }
+    } @@ TestAspect.flaky
 
+}
+
+object FakeServer {
+  type HttpServer = Fiber.Runtime[Throwable, Nothing]
+}
+
+trait FakeServer extends ZIOSpec[HttpServer] {
+
+  protected lazy val serverLayer = ZLayer.scoped(
+    ZIO.acquireRelease {
+      ZIO.logInfo(s"Starting server") *>
+        Server
+          .serve(Routes(Method.GET / "test" -> handler {
+            Response.text("test")
+          }).toHttpApp @@ Middleware.debug)
+          .provide(Server.default)
+          .fork
+    } { server =>
+      ZIO.logInfo(s"Stopping server") *>
+        server.interruptFork
+    }
+  )
+
+  override def bootstrap: ZLayer[Any, Throwable, HttpServer] =
+    serverLayer
 }
